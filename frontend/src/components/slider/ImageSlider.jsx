ae46ccf92cc7cf238ee5248011796bd69406ead9
Manjeet Rathore <20je0536@ee.iitism.ac.in>
2024-12-29 11:28:12 +0530

    first commit

---
+++ frontend/src/components/slider/ImageSlider.jsx
@@ -0,0 +1,37 @@
+// src/ImageSlider.js
+import React from 'react';
+import Slider from 'react-slick';
+import './slider.css';
+import NextArrow from './NextArrow'; // Import NextArrow component
+import PrevArrow from './PrevArrow'; // Import PrevArrow component
+
+export default function ImageSlider({ images = [] }) {
+    const settings = {
+        dots: false,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 4000,
+        arrows: true,
+        nextArrow: <NextArrow />,
+        prevArrow: <PrevArrow />,
+    };
+
+    const imageUrls = images.map((imageId) => 
+        `${import.meta.env.VITE_BACKEND_BASE_URL}/api/productImages/files/${imageId}`
+    );
+
+    return (
+        <div className="image-slider py-8">
+            <Slider {...settings}>
+                {imageUrls.map((url, index) => (
+                    <div key={index} className="product-image">
+                        <img src={url} alt={`Slide ${index + 1}`} />
+                    </div>
+                ))}
+            </Slider>
+        </div>
+    );
+}
---
+++ frontend/src/pages/Product.jsx
@@ -0,0 +1,82 @@
+import React, { useEffect, useState } from 'react';
+import { useParams, Link } from 'react-router-dom';
+import Navbar from '../components/navbar/Navbar';
+import ImageSlider from '../components/slider/ImageSlider';
+import ProductDetails from '../components/details/ProductDetails';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faArrowLeft, faShareNodes } from '@fortawesome/free-solid-svg-icons';
+import '../styles/product.css';
+import Loader from '../components/loader/Loader';
+import Review from '../components/review/Reviews';
+import Footer from '../components/footer/Footer';
+
+const Product = () => {
+
+    const handleShare = async () => {
+        if (navigator.share) {
+          try {
+            await navigator.share({
+              title: 'Check out this webpage!',
+              text: 'I found this interesting and wanted to share it with you.',
+              url: window.location.href,
+            });
+            console.log("Shared successfully");
+          } catch (error) {
+            console.error("Error sharing:", error);
+          }
+        } else {
+          console.log("Web Share API is not supported in this browser.");
+        }
+      };
+
+    window.scrollTo({
+        top: 60,
+        behavior: 'smooth',
+    });
+    const { id } = useParams();
+    const [product, setProduct] = useState(null);
+
+    useEffect(() => {
+        // Fetch the product data from your backend API
+        const fetchProduct = async () => {
+            try {
+                const response = await fetch(`${import.meta.env.VITE_BACKEND_BASE_URL}/api/allproducts/${id}`);
+                const data = await response.json();
+                setProduct(data);
+            } catch (error) {
+                console.error('Error fetching product data:', error);
+            }
+        };
+
+        fetchProduct();
+    }, [id]);
+
+    if (!product) {
+        return (<Loader />);
+    }
+
+    return (
+        <>
+            <Navbar />
+            <div className='product-row'>
+                <Link to="/" className='home-icon'>
+                    <FontAwesomeIcon icon={faArrowLeft} className='home' />
+                </Link>
+                <button onClick={handleShare} className='share-icon'>
+                    <FontAwesomeIcon icon={faShareNodes} className='share' />
+                </button>
+                <div className='relative'>
+                <ImageSlider images={product.images} />
+                </div>
+                <ProductDetails product={product} />  
+            </div>
+            <hr/>
+            <div className='reviews pb-4'>
+                <Review id={id} />
+            </div>
+            <Footer />
+        </>
+    );
+};
+
+export default Product;
