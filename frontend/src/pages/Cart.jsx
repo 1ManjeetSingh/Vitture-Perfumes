33e235830564cf1a5c65e8421e1c78b617848f77
Manjeet Rathore <20je0536@ee.iitism.ac.in>
2025-01-02 23:58:03 +0530

    commit 23:57 2/1

--- frontend/src/pages/Cart.jsx
+++ frontend/src/pages/Cart.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
 import Navbar from '../components/navbar/Navbar';
 import Footer from '../components/footer/Footer';
+import '../styles/cart.css'
 // import ProductListing from './ProductListing';
 
 const cartItems = [
@@ -49,13 +50,16 @@ const Cart = () => {
   return (
     <>
       <Navbar />
-      <div className="Cart flex gap-6 p-6 w-[90%] mx-auto">
-        <div className="cart-counter min-w-[320px] border h-fit p-4 flex flex-col items-start gap-6 bg-white">
-          <h1 className='text-xl'>Subtotal (2 items): <span className='font-bold'>₹1,298</span></h1>
-          <h1 className='text-lg'>Delivery: <span className='font-semibold'><del className='font-light'>₹40</del> free delivery ✅</span></h1>
-          <button className='rounded-full py-1 w-full text-md border border-[#FFCE12] bg-[#FFCE12] '>Proceed to Buy</button>
+      <div className="Cart flex px-6 pt-6 w-[90%] mx-auto relative">
+        <div className="cart-counter min-w-[320px] h-fit p-4 flex flex-col items-start gap-6 bg-white">
+          <h1 className='text-md sm:text-xl'>Subtotal (2 items): <span className='font-bold'>₹1,298</span></h1>
+          <h1 className='text-sm sm:text-lg'>Delivery: <span className='font-semibold'><del className='font-light'>₹40</del> free delivery ✅</span></h1>
+          <button className='rounded-full py-1 w-[95%] text-md border border-[#FFCE12] bg-[#FFCE12] font-semibold'>Proceed to Buy</button>
         </div>
-        <div className="w-full">
+        <div className='cart-counter-button hidden min-w-[320px] flex h-fit justify-center w-full bg-white py-3'>
+          <button className='rounded-full py-1 w-[95%] text-md border border-[#FFCE12] bg-[#FFCE12] font-semibold'>Proceed to Buy</button>
+        </div>
+        <div className="cart-items-container w-full px-6">
           {cartItems.map((item) => {
             return (
               <div key={item.id} className="cart-items flex w-full bg-white shadow-md rounded-lg relative p-4 mb-4">
@@ -63,20 +67,21 @@ const Cart = () => {
                   type="checkbox"
                   className="w-4 h-4 absolute top-4 left-4 cursor-pointer"
                 />
-                <div className="w-[160px] h-auto flex-shrink-0">
+                <div className="item-image w-[100px] sm:w-[160px] h-auto flex-shrink-0">
                   <img
                     src={item.image}
                     alt={item.itemName}
                     className="object-cover rounded-md"
                   />
                 </div>
-                <div className="flex flex-col grow gap-3 ml-6 justify-center">
-                  <h2 className="text-lg font-semibold">{item.itemName}</h2>
+                <div className="flex flex-col grow gap-3 ml-6 justify-center card-details">
+                  <h2 className="text-sm sm:text-lg font-semibold">{item.itemName}</h2>
+                  <h2 className="text-sm hidden sm:text-lg font-semibold">{`${item.itemName.slice(0, 40)}...`}</h2>
                   <div className='flex w-full gap-8'>
                     <div className='flex flex-col grow items-start'>
-                      <div className='flex items-center'>
-                        <span className='text-sm bg-red-600 text-white rounded-md p-1'>-{item.discount}%</span>
-                        <span className='text-md sm:text-lg font-semibold'>&nbsp;₹{discountedPrice(item.price, item.discount)}</span>
+                      <div className='flex items-center gap-1'>
+                        <span className='text-sm bg-red-600 text-white rounded-md p-[2px] sm:p-1'>-{item.discount}%</span>
+                        <span className='text-sm sm:text-lg font-semibold'>&nbsp;₹{discountedPrice(item.price, item.discount)}</span>
                       </div>
                       <div>
                         <span className='text-sm sm:text-lg font-light'>M.R.P.: <del>₹{item.price}</del></span>
@@ -85,24 +90,24 @@ const Cart = () => {
                     <div>
                     </div>
                   </div>
-                  <div className='flex items-center gap-6'>
+                  <div className='cart-items-bottom flex items-center gap-6'>
                     <div className="flex items-center grow space-x-4">
                       <button
                         onClick={() => handleDecrease(item.id)}
-                        className="w-8 h-8 bg-gray-200 hover:bg-gray-300 rounded-md text-lg flex items-center justify-center"
+                        className="w-5 h-5 sm:w-8 sm:h-8 bg-gray-200 hover:bg-gray-300 rounded-md text-sm sm:text-lg flex items-center justify-center"
                       >
                         -
                       </button>
-                      <span className="text-lg">{quantity}</span>
+                      <span className="text-sm sm:text-lg">{quantity}</span>
                       <button
                         onClick={() => handleIncrease(item.id)}
-                        className="w-8 h-8 bg-gray-200 hover:bg-gray-300 rounded-md text-lg flex items-center justify-center"
+                        className="w-5 h-5 sm:w-8 sm:h-8 bg-gray-200 hover:bg-gray-300 rounded-md text-sm sm:text-lg flex items-center justify-center"
                       >
                         +
                       </button>
                     </div>
-                    <div className='text-sm text-[#7796B6] flex gap-4 cursor-default overflow-hidden whitespace-nowrap text-ellipsis'>
-                      <span>Delete</span>
+                    <div className='cart-items-bottom-buttons text-sm text-[#7796B6] flex gap-4 cursor-default overflow-hidden whitespace-nowrap text-ellipsis'>
+                      <span >Delete</span>
                       <span>|</span>
                       <span>Save for later</span>
                       <span>|</span>
@@ -115,7 +120,7 @@ const Cart = () => {
           })}
         </div>
       </div>
-      <div className='w-full h-[40vh] bg-[#b1b1b1] mx-auto my-4 flex justify-center items-center'>
+      <div className='w-full p-2 bg-[#b1b1b1] mx-auto mb-4 flex justify-center items-center'>
         recomanded
       </div>
       <Footer />
--- frontend/src/pages/Profile.jsx
+++ frontend/src/pages/Profile.jsx
@@ -6,11 +6,25 @@ const Profile = () => {
   return (
     <>
     <Navbar />
-    <div className='min-h-[80vh] flex justify-center'>
-      <div className='flex flex-wrap'>
-
+    
+    <div className='flex w-full'>
+    <div className='h-[152vh] w-[220px] border border-gray-300 shadow-[0_0_10px_0_rgba(0,0,0,0.2)] flex flex-col gap-6'>
+      <div className='h-[92vh] flex flex-col w-full justify-start'
+      style={{
+        position: 'sticky',
+        top: '0px',
+      }}>
+      <div className='flex w-full justify-start border-b border-gray-300 p-2 text-lg font-semibold cursor-default bg-[#cfcfcf]'>Details</div>
+      <div className='flex w-full justify-start border-b border-gray-300 p-2 text-lg font-semibold cursor-default'>Your Orders</div>
+      <div className='flex w-full justify-start border-b border-gray-300 p-2 text-lg font-semibold cursor-default'>Address</div>
+      <div className='flex w-full justify-start border-b border-gray-300 p-2 text-lg font-semibold cursor-default'>payment methods</div>
       </div>
     </div>
+    <div>
+
+    </div>
+    </div>
+
     <Footer />
     </>
   )
